577684c613973911126388747e5125d3a1536dd5
f74034067 <f74034067@gmail.com>
2018-03-11 04:28:39 +0800

    Reduce the entry size

--- calculate.c
+++ calculate.c
@@ -11,7 +11,7 @@ int main(void)
     }
     int i = 0;
     char append[50], find[50];
-    double orig_sum_a = 0.0, orig_sum_f = 0.0, orig_a, orig_f;
+    /*double orig_sum_a = 0.0, orig_sum_f = 0.0, orig_a, orig_f;
     for (i = 0; i < 100; i++) {
         if (feof(fp)) {
             printf("ERROR: You need 100 datum instead of %d\n", i);
@@ -22,6 +22,7 @@ int main(void)
         orig_sum_a += orig_a;
         orig_sum_f += orig_f;
     }
+
     fclose(fp);
 
     fp = fopen("opt.txt", "r");
@@ -42,7 +43,26 @@ int main(void)
         fscanf(fp, "%s %s %lf %lf\n", append, find, &opt_a, &opt_f);
         opt_sum_a += opt_a;
         opt_sum_f += opt_f;
+    }*/
+    double orig_sum_a = 0.0, orig_sum_f = 0.0, orig_a, orig_f;
+    double opt_sum_a = 0.0, opt_sum_f = 0.0, opt_a, opt_f;
+    for (i = 0; i < 200; i++) {
+        if (feof(fp)) {
+            printf("ERROR: You need 100 datum instead of %d\n", i);
+            printf("run 'make run' longer to get enough information\n\n");
+            exit(0);
+        }
+        if (i<100) {
+            fscanf(fp, "%s %s %lf %lf\n", append, find, &orig_a, &orig_f);
+            orig_sum_a += orig_a;
+            orig_sum_f += orig_f;
+        } else {
+            fscanf(fp, "%s %s %lf %lf\n", append, find, &opt_a, &opt_f);
+            opt_sum_a += opt_a;
+            opt_sum_f += opt_f;
+        }
     }
+
     fprintf(output, "append() %lf %lf\n",orig_sum_a / 100.0, opt_sum_a / 100.0);
     fprintf(output, "findName() %lf %lf", orig_sum_f / 100.0, opt_sum_f / 100.0);
     fclose(output);
